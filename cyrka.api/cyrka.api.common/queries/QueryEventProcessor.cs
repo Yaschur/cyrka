14eedda00a834a236620bf1a5dddd1cd9748e698
Igor Trishin <yaschur@gmail.com>
2017-12-20 18:07:28 +0300

    start query processor

--- cyrka.api/cyrka.api.common/queries/QueryEventProcessor.cs
+++ cyrka.api/cyrka.api.common/queries/QueryEventProcessor.cs
@@ -1,7 +1,29 @@
+using System;
+using System.Reactive.Linq;
+using cyrka.api.common.events;
+
 namespace cyrka.api.common.queries
 {
 	public class QueryEventProcessor
 	{
+		public QueryEventProcessor(IEventStore eventStore, IQueryStore queryStore)
+		{
+			_eventStore = eventStore;
+			_queryStore = queryStore;
+		}
 
+		public void RegisterEventProcessing<TEvent, TQueryModel>(Func<TEvent, TQueryModel, TQueryModel> procFunc)
+		{
+			// TODO: manage subscribtions
+			_eventStore.AsObservable()
+				.Where(e => e is TEvent)
+				.Subscribe(e =>
+				{
+
+				});
+		}
+
+		private readonly IEventStore _eventStore;
+		private readonly IQueryStore _queryStore;
 	}
 }
