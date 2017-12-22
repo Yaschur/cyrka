581a3dc05e3330fae70d4ac888f9835ecc9f77a2
Igor Trishin <yaschur@gmail.com>
2017-12-22 10:27:27 +0300

    add test

--- cyrka.api/cyrka.api.test/common/QueryEventProcessorShould.cs
+++ cyrka.api/cyrka.api.test/common/QueryEventProcessorShould.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reactive.Subjects;
+using System.Threading.Tasks;
 using cyrka.api.common.events;
 using cyrka.api.common.queries;
 using FakeItEasy;
@@ -10,15 +14,44 @@ namespace cyrka.api.test.common
 	public class QueryEventProcessorShould
 	{
 		[Test]
-		public void DispatchEventsByRegister()
+		public async Task DispatchEventsByRegister()
 		{
 			var eventObservable = new Subject<Event>();
 			var eventStore = A.Fake<IEventStore>();
 			A.CallTo(() => eventStore.AsObservable())
 				.Returns(eventObservable);
 			var queryStore = A.Fake<IQueryStore>();
+			A.CallTo(() => queryStore.AsQueryable<object>())
+				.Returns(Enumerable.Empty<object>().AsQueryable());
 
-			// TODO...
+			var eventsProcessor = new QueryEventProcessor(eventStore, queryStore);
+			var (event1Counter, event2Counter) = (0, 0);
+			eventsProcessor.RegisterEventProcessing<EventData1, object>(
+				(ed, o) => { event1Counter++; return new object(); },
+				ed => o => true
+			);
+			eventsProcessor.RegisterEventProcessing<EventData1, object>(
+				(ed, o) => { event1Counter++; return new object(); },
+				ed => o => true
+			);
+			eventsProcessor.RegisterEventProcessing<EventData2, object>(
+				(ed, o) => { event2Counter++; return new object(); },
+				ed => o => true
+			);
+			var events = new List<Event> {
+				new Event(0, DateTime.UtcNow, new EventData2()),
+				new Event(0, DateTime.UtcNow, new EventData1()),
+				new Event(0, DateTime.UtcNow, new EventData3()),
+				new Event(0, DateTime.UtcNow, new EventData1()),
+				new Event(0, DateTime.UtcNow, new EventData1()),
+				new Event(0, DateTime.UtcNow, new EventData3()),
+				new Event(0, DateTime.UtcNow, new EventData2()),
+			};
+			events.ForEach(e => eventObservable.OnNext(e));
+			await Task.Delay(500);
+
+			Assert.AreEqual(6, event1Counter);
+			Assert.AreEqual(2, event2Counter);
 		}
 	}
 
