045b155342e6392b090ccb506516d6206d2ab271
Igor Trishin <yaschur@gmail.com>
2018-01-18 14:33:25 +0300

    (stacked with eventfdata)

--- cyrka.api/cyrka.api.web/Controllers/CustomersController.cs
+++ cyrka.api/cyrka.api.web/Controllers/CustomersController.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using cyrka.api.common.events;
 using cyrka.api.common.queries;
+using cyrka.api.domain.customers;
 using cyrka.api.domain.customers.commands.register;
+using cyrka.api.domain.customers.commands.registerTitle;
 using cyrka.api.domain.customers.queries;
 using cyrka.api.infra.nexter;
 using cyrka.api.web.Models.Customers;
@@ -21,16 +23,18 @@ namespace cyrka.api.web.Controllers
 		public CustomersController(
 			NexterGenerator nexterGenerator,
 			IEventStore eventStore,
-			IQueryStore queryStore
+			IQueryStore queryStore,
+			CustomerAggregateRepository customerRepository
 		)
 		{
 			_nexter = nexterGenerator;
 			_eventStore = eventStore;
 			_queryStore = queryStore;
+			_customerRepository = customerRepository;
 		}
 
 		[HttpPost]
-		public async Task Post([FromBody]CustomerInfo value)
+		public async Task<IActionResult> RegisterCustomer([FromBody]CustomerInfo value)
 		{
 			var command = new RegisterCustomer(value.Name, value.Description);
 			var commandHandler = new RegisterCustomerHandler();
@@ -39,11 +43,35 @@ namespace cyrka.api.web.Controllers
 			foreach (var data in eventDatas)
 			{
 				var lastEventId = await _eventStore.GetLastStoredId();
-				var id = await _nexter.GetNextNumber(EventChannelKey, lastEventId);
+				var newEventId = await _nexter.GetNextNumber(EventChannelKey, lastEventId);
 				var createdAt = DateTime.UtcNow;
-				var newEvent = new Event(id, createdAt, data);
+				var newEvent = new Event(newEventId, createdAt, data);
 				await _eventStore.Store(newEvent);
 			}
+
+			return Ok();
+		}
+
+		[HttpPost("{id}")]
+		public async Task<IActionResult> RegisterTitle(string id, [FromBody]TitleInfo value)
+		{
+			var customerAggregate = await _customerRepository.GetById(id);
+			if (customerAggregate == null)
+				return NotFound();
+			// var command = new RegisterTitle(id, value.Name, value.NumberOfSeries, value.Description);
+			// var commandHandler = new RegisterTitleHandler();
+			// var eventDatas = commandHandler.Handle(command);
+
+			// foreach (var data in eventDatas)
+			// {
+			// 	var lastEventId = await _eventStore.GetLastStoredId();
+			// 	var newEventId = await _nexter.GetNextNumber(EventChannelKey, lastEventId);
+			// 	var createdAt = DateTime.UtcNow;
+			// 	var newEvent = new Event(newEventId, createdAt, data);
+			// 	await _eventStore.Store(newEvent);
+			// }
+
+			return Ok();
 		}
 
 		[HttpGet]
@@ -55,5 +83,6 @@ namespace cyrka.api.web.Controllers
 		private readonly NexterGenerator _nexter;
 		private readonly IEventStore _eventStore;
 		private readonly IQueryStore _queryStore;
+		CustomerAggregateRepository _customerRepository;
 	}
 }
---
+++ cyrka.api/cyrka.api.web/Models/Customers/TitleInfo.cs
@@ -0,0 +1,9 @@
+namespace cyrka.api.web.Models.Customers
+{
+	public class TitleInfo
+	{
+		public string Name { get; set; }
+		public int NumberOfSeries { get; set; }
+		public string Description { get; set; }
+	}
+}
