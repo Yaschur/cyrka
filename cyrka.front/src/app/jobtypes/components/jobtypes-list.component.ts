b429cbe05879b70d412799137105a5222e177b76
Igor Trishin <yaschur@gmail.com>
2018-02-06 12:49:08 +0300

    add job types list

--- cyrka.front/src/app/jobtypes/components/jobtypes-list.component.ts
+++ cyrka.front/src/app/jobtypes/components/jobtypes-list.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Observable } from 'rxjs/Observable';
+
+import { JobTypesApiService } from '../services/jobtypes-api.service';
+import { JobTypePlain } from '../models/jobtype-plain';
 
 @Component({
 	selector: 'app-jobtypes-list',
 	templateUrl: './jobtypes-list.component.html',
 	styleUrls: ['./jobtypes-list.component.scss']
 })
-export class JobTypesListComponent implements OnInit {
+export class JobTypesListComponent {
 
-	constructor() { }
-
-	public ngOnInit() {
+	constructor(
+		private _jobTypesApi: JobTypesApiService,
+		private _router: Router
+	) {
+		this.jobTypes = _jobTypesApi.getAll();
 	}
 
+	public jobTypes: Observable<JobTypePlain[]>;
 }
