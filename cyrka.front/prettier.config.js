d8c29dbe47bf4e2b2a84dca48f351eb668209dea
Igor Trishin <yaschur@gmail.com>
2018-03-01 22:49:46 +0300

    ui: add prettier

---
+++ cyrka.front/prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	printWidth: 80,
+	tabWidth: 2,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'es5', // other options `es5` or `all`
+	bracketSpacing: true,
+	arrowParens: 'avoid', // other option 'always'
+	parser: 'typescript',
+	eslintIntegration: true,
+	stylelintIntegration: true,
+};
