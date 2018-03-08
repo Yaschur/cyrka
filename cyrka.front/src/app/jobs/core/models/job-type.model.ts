473c8db0e039e75a31714d86034a03eb32cbe1ab
Igor Trishin <yaschur@gmail.com>
2018-03-08 16:35:49 +0300

    change some...

---
+++ cyrka.front/src/app/jobs/core/models/job-type.model.ts
@@ -0,0 +1,15 @@
+export interface JobType {
+	id: string;
+	name: string;
+	unit: string;
+	rate: number;
+	description: string;
+}
+
+export interface JobTypeState extends JobType {
+	isNew: boolean;
+}
+
+export interface JobTypeList {
+	jobTypes: JobType[];
+}
