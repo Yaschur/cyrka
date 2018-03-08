9f9459a57a891efe23b8f2ca7f6c20b0646af37c
Igor Trishin <yaschur@gmail.com>
2018-03-08 16:40:31 +0300

    add getjobtypes action

--- cyrka.front/src/app/jobs/core/store/job-type.actions.ts
+++ cyrka.front/src/app/jobs/core/store/job-type.actions.ts
@@ -1,3 +1,9 @@
+import { Action } from '@ngrx/store';
+
 export const GET_JOBTYPES = 'GET_JOBTYPES';
 export const GET_JOBTYPES_SUCCESS = 'GET_JOBTYPES_SUCCESS';
 export const GET_JOBTYPES_ERROR = 'GET_JOBTYPES_ERROR';
+
+export class GetJobtypes implements Action {
+	readonly type = GET_JOBTYPES;
+}
