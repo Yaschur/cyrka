b18b9d24b3e39dabfc31e5fb566d9ef99dfb745f
Igor Trishin <yaschur@gmail.com>
2018-04-18 17:48:44 +0300

    ui: tune envs

--- cyrka.front/src/environments/environment.prod.ts
+++ cyrka.front/src/environments/environment.prod.ts
@@ -1,6 +1,13 @@
 export const environment = {
 	production: true,
 	cyrkaApi: {
-		baseUrl: 'cyrka-api'
-	}
+		baseUrl: 'cyrka-api',
+	},
+	auth: {
+		clientID: 'Omo0cOP8Dp6ycF5mFe1tRreoDibMBY0h',
+		domain: 'cyrka.eu.auth0.com',
+		audience: 'http://u.cyrillica.ru:43080/cyrka-api/',
+		redirect: 'http://u.cyrillica.ru:43080/callback',
+		scope: 'openid profile email',
+	},
 };
