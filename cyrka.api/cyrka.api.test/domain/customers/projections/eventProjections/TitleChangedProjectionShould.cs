7934300d0fd9e932b4f20b1e0813d311d87cc03e
Igor Trishin <yaschur@gmail.com>
2018-07-24 17:40:51 +0300

    api: add tests

--- cyrka.api/cyrka.api.test/domain/customers/projections/eventProjections/TitleChangedProjectionShould.cs
+++ cyrka.api/cyrka.api.test/domain/customers/projections/eventProjections/TitleChangedProjectionShould.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Linq;
+using cyrka.api.common.projections.results;
 using cyrka.api.domain.customers.commands.changeTitle;
 using cyrka.api.domain.customers.projections.eventProjections;
 using cyrka.api.domain.customers.projections.viewModels;
@@ -9,45 +11,15 @@ namespace cyrka.api.test.domain.customers.projections.eventProjections
 	[TestFixture]
 	public class TitleChangedProjectionShould
 	{
-		// TODO: Do not change others, Do not change if missing or empty
-
 		[Test]
 		public void ChangeAppropriateTitleInTargetView()
 		{
-			var targetView = new CustomerFullView
-			{
-				Id = TestContext.CurrentContext.Random.GetString(),
-				Name = TestContext.CurrentContext.Random.GetString(),
-				Description = TestContext.CurrentContext.Random.GetString(),
-				TitlesCount = 3,
-				Titles = new List<TitleView>
-				{
-					new TitleView
-					{
-						Id = TestContext.CurrentContext.Random.GetString(),
-						Name = TestContext.CurrentContext.Random.GetString(),
-						Description = TestContext.CurrentContext.Random.GetString(),
-						NumberOfSeries = TestContext.CurrentContext.Random.Next(1),
-					},
-					new TitleView
-					{
-						Id = TestContext.CurrentContext.Random.GetString(),
-						Name = TestContext.CurrentContext.Random.GetString(),
-						Description = TestContext.CurrentContext.Random.GetString(),
-						NumberOfSeries = TestContext.CurrentContext.Random.Next(1),
-					},
-					new TitleView
-					{
-						Id = TestContext.CurrentContext.Random.GetString(),
-						Name = TestContext.CurrentContext.Random.GetString(),
-						Description = TestContext.CurrentContext.Random.GetString(),
-						NumberOfSeries = TestContext.CurrentContext.Random.Next(1),
-					},
-				}
-			};
+			var targetView = GenerateCustomerFullView(TestContext.CurrentContext.Random.Next(1, 9999));
+			var targetIndex = TestContext.CurrentContext.Random.Next(1, targetView.TitlesCount);
+			var targetTitle = targetView.Titles[targetIndex];
 			var eventData = new TitleChanged(
 				targetView.Id,
-				targetView.Titles[1].Id,
+				targetTitle.Id,
 				TestContext.CurrentContext.Random.GetString(),
 				TestContext.CurrentContext.Random.Next(1),
 				TestContext.CurrentContext.Random.GetString()
@@ -55,12 +27,102 @@ namespace cyrka.api.test.domain.customers.projections.eventProjections
 			var eventProjectionUnderTest = new TitleChangedProjection();
 
 			var can = eventProjectionUnderTest.CanProject(eventData);
-			var res = eventProjectionUnderTest.Project(eventData, targetView);
+			eventProjectionUnderTest.Project(eventData, targetView);
 
 			Assert.IsTrue(can);
-			Assert.AreEqual(eventData.Name, targetView.Titles[1].Name);
-			Assert.AreEqual(eventData.NumberOfSeries, targetView.Titles[1].NumberOfSeries);
-			Assert.AreEqual(eventData.Description, targetView.Titles[1].Description);
+			Assert.AreEqual(eventData.Name, targetTitle.Name);
+			Assert.AreEqual(eventData.NumberOfSeries, targetTitle.NumberOfSeries);
+			Assert.AreEqual(eventData.Description, targetTitle.Description);
+		}
+
+		[Test]
+		public void ProjectStoreProjectionResult()
+		{
+			var targetView = GenerateCustomerFullView(TestContext.CurrentContext.Random.Next(1, 9999));
+			var targetIndex = TestContext.CurrentContext.Random.Next(1, targetView.TitlesCount);
+			var targetTitle = targetView.Titles[targetIndex];
+			var eventData = new TitleChanged(
+				targetView.Id,
+				targetTitle.Id,
+				TestContext.CurrentContext.Random.GetString(),
+				TestContext.CurrentContext.Random.Next(1),
+				TestContext.CurrentContext.Random.GetString()
+			);
+			var eventProjectionUnderTest = new TitleChangedProjection();
+
+			var res = eventProjectionUnderTest.Project(eventData, targetView);
+
+			Assert.IsInstanceOf<StoreProjectionResult<CustomerFullView>>(res);
+		}
+
+		[Test]
+		public void NotChangeOtherTitlesInTargetView()
+		{
+			var targetView = GenerateCustomerFullView(TestContext.CurrentContext.Random.Next(2, 9999));
+			var targetIndex = TestContext.CurrentContext.Random.Next(1, targetView.TitlesCount);
+			var targetTitleId = targetView.Titles[targetIndex].Id;
+			var eventData = new TitleChanged(
+				targetView.Id,
+				targetTitleId,
+				TestContext.CurrentContext.Random.GetString(),
+				TestContext.CurrentContext.Random.Next(1),
+				TestContext.CurrentContext.Random.GetString()
+			);
+			var eventProjectionUnderTest = new TitleChangedProjection();
+
+			eventProjectionUnderTest.Project(eventData, targetView);
+
+			foreach (var ot in targetView.Titles.Where(t => t.Id != targetTitleId))
+			{
+				Assert.AreNotEqual(eventData.Name, ot.Name);
+				Assert.AreNotEqual(eventData.NumberOfSeries, ot.NumberOfSeries);
+				Assert.AreNotEqual(eventData.Description, ot.Description);
+			}
+		}
+
+		[Test]
+		public void NotChangeTitlesIfNoAppropriateById()
+		{
+			var targetView = GenerateCustomerFullView(TestContext.CurrentContext.Random.Next(0, 9999));
+			var eventData = new TitleChanged(
+				targetView.Id,
+				TestContext.CurrentContext.Random.GetString(),
+				TestContext.CurrentContext.Random.GetString(),
+				TestContext.CurrentContext.Random.Next(1),
+				TestContext.CurrentContext.Random.GetString()
+			);
+			var eventProjectionUnderTest = new TitleChangedProjection();
+
+			eventProjectionUnderTest.Project(eventData, targetView);
+
+			foreach (var ot in targetView.Titles)
+			{
+				Assert.AreNotEqual(eventData.Name, ot.Name);
+				Assert.AreNotEqual(eventData.NumberOfSeries, ot.NumberOfSeries);
+				Assert.AreNotEqual(eventData.Description, ot.Description);
+			}
+		}
+
+		private CustomerFullView GenerateCustomerFullView(int titleCount)
+		{
+			var resultView = new CustomerFullView
+			{
+				Id = TestContext.CurrentContext.Random.GetString(),
+				Name = TestContext.CurrentContext.Random.GetString(),
+				Description = TestContext.CurrentContext.Random.GetString(),
+				TitlesCount = titleCount,
+				Titles = Enumerable.Range(0, titleCount)
+					.Select(_ => new TitleView
+					{
+						Id = TestContext.CurrentContext.Random.GetString(),
+						Name = TestContext.CurrentContext.Random.GetString(),
+						Description = TestContext.CurrentContext.Random.GetString(),
+						NumberOfSeries = TestContext.CurrentContext.Random.Next(),
+					})
+					.ToList()
+			};
+
+			return resultView;
 		}
 	}
 }
