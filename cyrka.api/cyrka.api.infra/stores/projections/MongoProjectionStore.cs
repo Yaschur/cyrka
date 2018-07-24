bae13478e5b9a84f9351bd8167bada7d40e68f30
Igor Trishin <yaschur@gmail.com>
2018-07-24 22:13:04 +0300

    api: implement mongo projection store

--- cyrka.api/cyrka.api.infra/stores/projections/MongoProjectionStore.cs
+++ cyrka.api/cyrka.api.infra/stores/projections/MongoProjectionStore.cs
@@ -1,24 +1,61 @@
+using System.Linq;
 using System.Threading.Tasks;
 using cyrka.api.common.projections;
+using MongoDB.Driver;
 
 namespace cyrka.api.infra.stores.projections
 {
 	public class MongoProjectionStore<TView> : IProjectionStore<TView>
 		where TView : IView
 	{
+		public MongoProjectionStore(IMongoDatabase mongoDatabase)
+		{
+			var collectionName = typeof(TView).Name;
+			_mongoCollection = mongoDatabase.GetCollection<TView>(collectionName);
+		}
+
 		public TView GetById(string id)
 		{
-			throw new System.NotImplementedException();
+			try
+			{
+				return _mongoCollection
+					.AsQueryable()
+					.FirstOrDefault(v => v.Id == id);
+			}
+			catch (System.Exception)
+			{
+				throw;
+			}
 		}
 
-		public Task RemoveAsync(TView view)
+		public async Task RemoveAsync(TView view)
 		{
-			throw new System.NotImplementedException();
+			try
+			{
+				await _mongoCollection.DeleteOneAsync(v => view.Id == v.Id);
+			}
+			catch (System.Exception)
+			{
+				throw;
+			}
 		}
 
-		public Task StoreAsync(TView view)
+		public async Task StoreAsync(TView view)
 		{
-			throw new System.NotImplementedException();
+			try
+			{
+				await _mongoCollection.ReplaceOneAsync(
+				 v => v.Id == view.Id,
+				 view,
+				 new UpdateOptions { IsUpsert = true }
+			 );
+			}
+			catch (System.Exception)
+			{
+				throw;
+			}
 		}
+
+		private readonly IMongoCollection<TView> _mongoCollection;
 	}
 }
